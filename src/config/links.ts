bf5f289e4ea8d3e941f7d68e23a67e02da4eb5a4
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-04-20 16:24:03 -0700

    add opensea

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,63 +1,82 @@
-import { faDev, faDiscord, faDribbble, faGithub, faGitlab, faInstagram, faLinkedinIn, faTwitch, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faGlobe, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import {
+	faDev,
+	faDiscord,
+	faDribbble,
+	faGithub,
+	faGitlab,
+	faInstagram,
+	faLinkedinIn,
+	faTwitch,
+	faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+	faEnvelope,
+	faGlobe,
+	faAddressCard,
+	faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
 
-import details from './details';
+import details from "./details";
 
 const links = [
-
 	{
-		text: 'GitHub',
+		text: "GitHub",
 		icon: faGithub,
-		url: 'https://github.com/Thomashighbaugh/',
+		url: "https://github.com/Thomashighbaugh/",
 	},
 	{
-		text: 'GitLab',
+		text: "GitLab",
 		icon: faGitlab,
-		url: 'https://gitlab.com/Thomashighbaugh/',
+		url: "https://gitlab.com/Thomashighbaugh/",
 	},
 	{
-		text: 'Dev.to',
+		text: "Dev.to",
 		icon: faDev,
-		url: 'dev.to/thomashighbaugh',
+		url: "dev.to/thomashighbaugh",
 	},
 	{
-		text: 'Discord',
+		text: "Discord",
 		icon: faDiscord,
-		url: 'https://discord.com/ThomasHighbaugh#3886'
+		url: "https://discord.com/ThomasHighbaugh#3886",
 	},
 
 	{
-		text: 'Dribbble',
+		text: "Dribbble",
 		icon: faDribbble,
-		url:'https://dribbble.com/thighbaugh'
+		url: "https://dribbble.com/thighbaugh",
 	},
 	{
-		text: 'Instagram',
+		text: "Instagram",
 		icon: faInstagram,
-		url: 'https://www.instagram.com/thomashighbaugh/',
+		url: "https://www.instagram.com/thomashighbaugh/",
 	},
 	{
-		text: 'Twitter',
+		text: "OpenSea",
+		icon: faShoppingCart,
+		url: "https://opensea.io/Thomashighbaugh",
+	},
+	{
+		text: "Twitter",
 		icon: faTwitter,
-		url: 'https://twitter.com/thomashighbaugh'
+		url: "https://twitter.com/thomashighbaugh",
 	},
 	{
-		text: 'LinkedIn',
+		text: "LinkedIn",
 		icon: faLinkedinIn,
-		url: 'https://www.linkedin.com/in/thomas-leon-highbaugh/',
+		url: "https://www.linkedin.com/in/thomas-leon-highbaugh/",
 	},
 	{
-		text: 'Resume',
+		text: "Resume",
 		icon: faAddressCard,
-		url: 'https://resume-thomas-leon-highbaugh.vercel.app/',
+		url: "https://resume-thomas-leon-highbaugh.vercel.app/",
 	},
 	{
-		text: 'Website',
+		text: "Website",
 		icon: faGlobe,
-		url: 'https://thomasleonhighbaugh.me',
+		url: "https://thomasleonhighbaugh.me",
 	},
 	{
-		text: 'E-mail me',
+		text: "E-mail me",
 		icon: faEnvelope,
 		url: `mailto:${details.email}`,
 	},
