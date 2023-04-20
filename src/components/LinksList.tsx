ebf1b8229c2e56f1dedd3c227d4d8aef0d612cb1
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-04-20 16:43:00 -0700

    backtrack

--- src/components/LinksList.tsx
+++ src/components/LinksList.tsx
@@ -4,13 +4,13 @@ import links from '../config/links';
 
 export default function LinksList() {
 	return (
-		<ul className='w-full h-full  gap-y-4 mt-3 flex flex-col flex-wrap sm:flex-row md:flex-row lg:flex-row xl:flex-row -m-2'>
+		<ul className='w-full h-full  gap-y-4 mt-3 flex flex-col flex-wrap sm:flex-row md:flex-row lg:flex-row xl:flex-row -mx-2'>
 			{links.map((link) => (
 				<li
 					key={link.text}
 					title={link.text}
 					tabIndex={0}
-					className=' mb-0 sm:mb-0 md:mb-4 px-2 min-w-[28%] sm:mx-0 mx-0 md:mx-4 lg:mx-4 xl:mx-4 rounded bg-accent-primary transition-all duration-200 hover:bg-accent-primary-state hover:-translate-y-2
+					className=' mb-4 sm:mb-0 md:mb-4 px-2 min-w-[28%] sm:mx-0 mx-4 md:mx-4 lg:mx-4 xl:mx-4 rounded bg-accent-primary transition-all duration-200 hover:bg-accent-primary-state hover:-translate-y-2
                 focus:bg-accent-primary-state focus:-translate-y-2'
 				>
 					<a
@@ -24,7 +24,7 @@ export default function LinksList() {
 							icon={link.icon}
 							className='w-10 h-10 hover:text-color-light absolute  m-10 text-color-dark'
 						/>
-						<span className='mx-auto text-color-dark invisible sm:visible md:invisible lg:invisible xl:invisible hover:text-color-light text-xl'>
+						<span className='mx-auto text-color-dark md:invisible lg:invisible xl:invisible hover:text-color-light text-xl'>
 							{link.text}
 						</span>
 					</a>
