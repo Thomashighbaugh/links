754560b4353a4be17a1ee1237e2d22948ae72689
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-04-20 17:00:03 -0700

    group hover

--- src/components/LinksList.tsx
+++ src/components/LinksList.tsx
@@ -10,21 +10,21 @@ export default function LinksList() {
 					key={link.text}
 					title={link.text}
 					tabIndex={0}
-					className=' mb-4 sm:mb-0 md:mb-4 px-2 min-w-[28%] sm:mx-0 mx-4 md:mx-4 lg:mx-4 xl:mx-4 rounded bg-accent-primary transition-all duration-200 hover:bg-accent-primary-state hover:-translate-y-2
-                focus:bg-accent-primary-state focus:-translate-y-2 hover:drop-shadow-2xl hover:shadow-2xl hover:shadow-inner'
+					className=' mb-4 sm:mb-0 md:mb-4 px-2 min-w-[28%] sm:mx-0 mx-4 md:mx-4 lg:mx-4 xl:mx-4 rounded bg-accent-primary transition-all duration-400 hover:bg-accent-primary-state hover:-translate-y-2
+                focus:bg-accent-primary-state focus:-translate-y-2 hover:drop-shadow-2xl hover:shadow-2xl hover:shadow-inner group'
 				>
 					<a
 						href={link.url}
 						target='_blank'
 						rel='noopener'
 						tabIndex={-1}
-						className='w-full h-full flex items-center p-3 rounded hover:text-color-light'
+						className='w-full h-full flex items-center p-3 rounded group-hover:text-color-light'
 					>
 						<FontAwesomeIcon
 							icon={link.icon}
-							className='w-10 h-10 hover:text-color-light absolute  m-10 text-color-dark'
+							className='w-10 h-10 group-hover:text-color-light absolute  m-10 text-color-dark'
 						/>
-						<span className='mx-auto text-color-dark md:invisible lg:invisible xl:invisible hover:text-color-light text-xl'>
+						<span className='mx-auto text-color-dark md:invisible lg:invisible xl:invisible group-hover:text-color-light text-xl'>
 							{link.text}
 						</span>
 					</a>
