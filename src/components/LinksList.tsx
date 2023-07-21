866eb94d37c3665ec9e5d92084cb4ab91e7ea141
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-07-20 20:01:45 -0700

    fix rendered issues and graph issues

--- src/components/LinksList.tsx
+++ src/components/LinksList.tsx
@@ -4,13 +4,13 @@ import links from "../config/links";
 
 export default function LinksList() {
 	return (
-		<ul className='w-full h-full  gap-y-4 mt-3 flex flex-col flex-wrap sm:flex-row md:flex-row lg:flex-row xl:flex-row -mx-2'>
+		<ul className='w-full h-full p-2  gap-y-4 mt-6 flex flex-col flex-wrap sm:flex-row md:flex-row lg:flex-row xl:flex-row -mx-4 overflow-hidden'>
 			{links.map((link) => (
 				<li
 					key={link.text}
 					title={link.text}
 					tabIndex={0}
-					className=' mb-4 sm:mb-0 md:mb-4 px-2 min-w-[28%] sm:mx-0 mx-4 md:mx-4 lg:mx-4 xl:mx-4 rounded bg-accent-primary transition-all duration-400 hover:bg-accent-primary-state hover:-translate-y-2
+					className=' mb-4 sm:mb-0 md:mb-4 px-2 min-w-[28%] lg:max-w-[28%] xl:max-w-[28%] md:max-w-[28%] sm:mx-0 mx-2 md:mx-2 lg:mx-2 xl:mx-2 rounded bg-accent-primary transition-all duration-400 hover:bg-accent-primary-state hover:-translate-y-2
                 focus:bg-accent-primary-state focus:-translate-y-2 hover:drop-shadow-2xl hover:shadow-2xl hover:shadow-inner group'
 				>
 					<a
@@ -18,11 +18,11 @@ export default function LinksList() {
 						target='_blank'
 						rel='noopener'
 						tabIndex={-1}
-						className='w-full h-full flex items-center p-3 rounded group-hover:text-color-light'
+						className='w-full h-full max-x-full max-h-full flex items-center p-4 rounded group-hover:text-color-light'
 					>
 						<FontAwesomeIcon
 							icon={link.icon}
-							className='w-10 h-10 group-hover:text-color-light absolute  m-10 text-color-dark'
+							className='w-12 h-12 max-h-12 max-w-12 group-hover:text-color-light absolute  mx-8 text-color-dark'
 						/>
 						<span className='mx-auto text-color-dark md:invisible lg:invisible xl:invisible group-hover:text-color-light text-xl'>
 							{link.text}
