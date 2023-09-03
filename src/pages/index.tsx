5171c9bdd4691c6d17b6ee00e05ad0a3876b4c57
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-09-03 00:59:17 -0700

    browsers suck

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 		<>
 			<MetaHead />
 
-			<main className='w-full max-w-6xl h-full flex flex-col justify-between items-center m-auto pt-12'>
+			<main className='w-full max-w-6xl h-full flex flex-col justify-center items-center mx-auto pt-12'>
 				<Header />
 				<LinksList />
 				<Footer />
