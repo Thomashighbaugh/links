9ed229574880cdc8b0c27cd7c521e35b79bd1088
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-11-02 08:54:33 -0700

    fixed list padding

--- src/components/LinksList.tsx
+++ src/components/LinksList.tsx
@@ -4,7 +4,7 @@ import links from '../config/links';
 
 export default function LinksList() {
 	return (
-		<ul className='w-full h-full grow gap-x-4  gap-y-4 p-6 flex flex-col flex-wrap sm:flex-col md:flex-row lg:flex-row xl:flex-row -mx-4 overflow-hidden'>
+		<ul className='w-full h-full grow gap-x-4  gap-y-4 pb-24 px-16 pt-6  flex flex-col flex-wrap sm:flex-col md:flex-row lg:flex-row xl:flex-row -mx-4 overflow-hidden'>
 			{links.map((link) => (
 				<li
 					key={link.text}
